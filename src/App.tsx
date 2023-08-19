9edc8c75803b23c39d107310bc80e7ae3b98a27e
k3rnel_panic <shotov1303@gmail.com>
2023-08-20 02:08:01 +0400

    infinite scrolling added

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import CountryDropdown from './components/controls/CountryDropdown';
 import Navbar from './components/navbar/Navbar';
 import Controls from './components/controls/Controls';
 import { v4 as uuid4} from 'uuid';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
   const [users, setUsers] = useState<any[]>([]);
   const [country, setCountry] = useState("USA");
+  const [page, setPage] = useState(1);
   
   const countryCodes: { [key: string]: string } = {
     "Georgia": "GE",
@@ -18,6 +20,7 @@ function App() {
 
   const handleCountryChange = (selectedCountry: string) => {
     setCountry(selectedCountry);
+    setPage(1);
   }
 
   type FakerType = typeof fakerEN_US | typeof fakerPL | typeof fakerKA_GE;
@@ -59,11 +62,14 @@ function App() {
         selectedFaker = fakerEN_US;
         break;
     }
-  
     const generate = generateUser(selectedFaker);
     const generatedUsers = Array.from({ length: 20 }, () => generate());
-    setUsers(generatedUsers);
-  }, [country]);
+    if (page === 1) {
+      setUsers(generatedUsers);
+    } else {
+      setUsers(prevUsers => [...prevUsers, ...generatedUsers]);
+    }
+  }, [country, page]);
   
 
   return (
@@ -75,6 +81,12 @@ function App() {
           <CountryDropdown handleCountryChange={handleCountryChange} />
         </div>
         <div className="row justify-content-center">
+        <InfiniteScroll
+          dataLength={users.length} 
+          next={() => setPage(prevPage => prevPage + 1)} 
+          hasMore={true} 
+          loader={<h4>Loading more...</h4>}
+        >
           {users.map(user => (
             <div className="col-12 col-md-6 offset-md-3 mb-2" key={user.id}>
               
@@ -82,10 +94,11 @@ function App() {
               
             </div>  
           ))}
-        </div>
-        
+        </InfiniteScroll>
+      </div>
     </div>
-  );
+        
+  )
 }
 
 export default App;
